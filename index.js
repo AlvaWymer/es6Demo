68ed86ba6b68cddc2fd9096fb721180847e4cf04
yangnaihua <yangnaihua.2008@163.com>
2017-10-18 21:11:52 +0200

    test 字符串拼接

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ let cala = 1;
 let calb = 2; 
 let resule = `${cala + calb}`;
 console.log(resule);
+
+// 是否包含
+let incla = 'yang';
+let inclus = 'yangnaihfdusfhsduifhsauifhdsia';
+console.log(inclus.includes(incla)); // 是否包含
+// 是否以这个开头
+console.log(inclus.startsWith(incla));
+//是否以这个结尾
+console.log(inclus.endsWith(incla));
+console.log(`haha|`.repeat(3));
